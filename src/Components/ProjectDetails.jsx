29fe99db3333cef78a625da3368eeb4e6077dfe3
Rishita P <pram.73ris@gmail.com>
2024-12-25 23:28:12 +0530

    ui updated

--- src/Components/ProjectDetails.jsx
+++ src/Components/ProjectDetails.jsx
@@ -38,7 +38,7 @@ const ProjectDetails = ({ whiteMode }) => {
                 className="my-10 w-[90%] xl:w-[70%] flex flex-col items-center justify-center"
                 key={i}
               >
-                <figure className="w-full h-auto md:h-[550px] pb-0 lg:pb-6">
+                <figure className="w-full h-auto md:h-[560px] pb-0 lg:pb-6">
                   <img
                     src={d?.image}
                     className="object-contain lg:object-fill w-full h-full rounded-lg"
@@ -60,11 +60,11 @@ const ProjectDetails = ({ whiteMode }) => {
                           className="my-2 flex flex-col text-lg lg:text-xl"
                           key={i}
                         >
-                          <span className="py-1 text-[#707070] font-semibold">
+                          <span className="text-lg lg:text-xl py-1 text-[#707070] font-semibold">
                             {x?.title}
                           </span>
                           <span
-                            className={`${
+                            className={`text-lg lg:text-xl ${
                               whiteMode ? "text-[#101010]" : "text-[#fff]"
                             }`}
                           >
@@ -139,7 +139,7 @@ const ProjectDetails = ({ whiteMode }) => {
                       }}
                     ></p>
                     {d?.image && (
-                      <figure className="w-full h-auto md:h-[450px] py-2 xl:py-6">
+                      <figure className="w-full h-auto md:h-[550px] py-2 xl:py-6">
                         <img
                           src={d?.image}
                           className="object-contain lg:object-fill rounded-lg w-full h-full"
@@ -161,7 +161,8 @@ const ProjectDetails = ({ whiteMode }) => {
         >
           More to explore
         </h2>
-        <section className="grid grid-cols-1 md:grid-cols-2  gap-10 mb-20">
+        {/* web */}
+        <section className="hidden lg:grid grid-cols-1 md:grid-cols-2  gap-10 mb-20">
           {moreProjects?.slice(0, 2)?.map((d, i) => {
             return (
               <section className="flex flex-col items-start" key={i}>
@@ -172,7 +173,7 @@ const ProjectDetails = ({ whiteMode }) => {
                     className="object-contain md:object-fill rounded-2xl w-full h-auto md:h-[400px]"
                   />
                 </figure>
-                <p className="py-1.5 text-3xl text-[#e8ca42] font-caveat">
+                <p className="py-1.5 text-2xl text-[#e8ca42] font-caveat">
                   {d?.type}
                 </p>
                 <h3
@@ -203,6 +204,52 @@ const ProjectDetails = ({ whiteMode }) => {
             );
           })}
         </section>
+        {/* mobile */}
+        {moreProjects?.slice(0, 2)?.map((d, i) => {
+          return (
+            <section
+              key={i}
+              className={`lg:hidden flex flex-col rounded-[28px] w-[95%] my-4  mx-auto cursor-pointer bg-gray-100 `}
+            >
+              <figure className={`rounded-t-[28px] w-full h-full`}>
+                <img
+                  src={d?.img}
+                  alt="project"
+                  className="object-contain w-full h-full rounded-t-[28px]"
+                />
+              </figure>
+
+              <section
+                className={`flex items-center justify-center flex-col p-5`}
+              >
+                <p
+                  className={`text-center py-1 font-caveat font-semibold rounded-[30px] w-fit text-lg sm:text-xl text-[#ffd859] `}
+                >
+                  {d?.type}
+                </p>
+                <h3 className={`py-1 font-bold !text-2xl text-center`}>
+                  {d?.title}
+                </h3>
+                <p
+                  className="py-3 flex items-start justify-center text-lg cursor-pointer group"
+                  onClick={() => {
+                    window.scroll({ top: 0, left: 0, behavior: "smooth" });
+                    navigate(`/project-details/${d?.slug}`);
+                  }}
+                >
+                  <span
+                    className={`group-hover:underline font-semibold 
+                    ${whiteMode ? "text-[#525155]" : "text-[#D1D1D1]"}
+                    `}
+                  >
+                    See My Work
+                  </span>
+                  <img src={iconLink} alt="link" className="ml-2 w-8 h-8" />
+                </p>
+              </section>
+            </section>
+          );
+        })}
       </Container>
     </>
   );
